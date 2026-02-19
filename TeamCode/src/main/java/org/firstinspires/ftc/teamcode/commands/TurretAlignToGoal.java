fa8f5584bb279a83d4f4f2f750e8b132121a49ce
AceElephants <elephants19766@gmail.com>
2026-02-19 23:14:09 +0200

    turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TurretAlignToGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TurretAlignToGoal.java
@@ -1,18 +1,33 @@
 package org.firstinspires.ftc.teamcode.commands;
 
+import com.arcrobotics.ftclib.geometry.Vector2d;
+
 import org.firstinspires.ftc.teamcode.subsystem.GaryDrivetrain;
 import org.firstinspires.ftc.teamcode.subsystem.Turret;
+import org.firstinspires.ftc.teamcode.utils.Constants;
 
 import java.util.function.DoubleSupplier;
 
 public class TurretAlignToGoal extends TurretGetToAngle {
 
-    public TurretAlignToGoal(Turret turret, GaryDrivetrain garyDrivetrain) {
-        super(
-                turret,
-                        () -> {
-                            Math.toDegrees()
-                        }
+    public TurretAlignToGoal(Turret turret, GaryDrivetrain garyDrivetrain, boolean isGoalBlue) {
+        super(turret, () -> {
+                    Vector2d goalPose = isGoalBlue ? Constants.BLUE_GOAL_POS : Constants.RED_GOAL_POS;
+
+                    double absAngle = Math.atan2(
+                            goalPose.getY() - garyDrivetrain.getFollower().getPose().getY(),
+                            goalPose.getX() - garyDrivetrain.getFollower().getPose().getX()
+                    );
+
+                    double turretAngle = Math.toDegrees(
+                            absAngle - garyDrivetrain.getFollower().getHeading()
+                    );
+
+                    turretAngle = (turretAngle > 180) ? (turretAngle - 360) : turretAngle;
+                    turretAngle = (turretAngle < 180) ? (turretAngle + 360) : turretAngle;
+
+                    return turretAngle;
+                }
         );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Constants.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import com.arcrobotics.ftclib.geometry.Vector2d;
+
 public class Constants {
 
+    public static final Vector2d RED_GOAL_POS = new Vector2d(144,144);
+    public static final Vector2d BLUE_GOAL_POS = new Vector2d(0,144);
 
 }
