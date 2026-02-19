2b02a9d7001303d9468ceab5dfd44977190bddc9
Nevo <nevoday2006@gmail.com>
2026-02-20 01:29:21 +0200

    publiceted the Drawing class.
     wanted it to draw the robot outside of Tuning. used it in GaryDrivetrain's periodic (need to be tested if works).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/GaryDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/GaryDrivetrain.java
@@ -1,29 +1,29 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.pedropathing.follower.Follower;
-import com.qualcomm.hardware.bosch.BNO055IMUNew;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.pedroPathing.Drawing;
 
 public class GaryDrivetrain extends SubsystemBase {
 
     private final Follower follower;
 
-
     public GaryDrivetrain(HardwareMap hardwareMap) {
+        super(); //registers this subsystem (for the periodic block below to be called).
         follower = Constants.createFollower(hardwareMap);
     }
 
     public Follower getFollower() {
         return follower;
+    }
+
+    @Override
+    public void periodic() {
+        follower.update();
+        Drawing.drawDebug(follower);
+        Drawing.sendPacket();
     }
 }
